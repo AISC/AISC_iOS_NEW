41e7c9a8a7fedde3eea025aa5529207dedadf24d
Zach Whelchel <zach.whelchel@gmail.com>
2014-11-17 10:59:02 +0530

    Initial Commit

---
+++ AISC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AISC
+//
+//  Created by Zach Whelchel on 11/17/14.
+//  Copyright (c) 2014 AISC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
